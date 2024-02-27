af0838d1c2a6181984ae67f5ecc3e64abf272ebd
SHIMHUN <shimhun88@naver.com>
2024-02-28 01:36:11 +0900

    의존관계 자동 주입 테스트

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.lang.module.Configuration;
 
 // 스프링 부트 어노테이션 안에 컴포넌트 스캔이 있으므로 @SpringBootApplication 어노테이션이 있는 곳의 패키지로부터  모든 빈들을 읽음. 따라서 AutoAppConfig에서 configuration을 exclude한 설정은 적용되지 않음 why? 이미 컴포넌트 스캔을 다 해주므로
-@SpringBootApplication( exclude = Configuration.class)
+//@SpringBootApplication( exclude = Configuration.class)
+@SpringBootApplication
 public class CoreApplication {
 
 	public static void main(String[] args) {
